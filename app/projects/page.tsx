7ef70ef9421ca972cdabccbe0396e984575eafd3
chris-stallings <164257968+chris-stallings@users.noreply.github.com>
2025-06-10 13:46:17 -0400

    fixed navbar

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,9 +2,8 @@ import Image from "next/image";
 
 export default function Projects() {
   return (
-    <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-  </div>
-</div>
+    <div>
+      
+    </div>
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { FaLinkedin } from 'react-icons/fa'
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
 
 const Footer = () => {
   return (
     <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
   <aside className="items-center">
-    <p>Copyright © {new Date().getFullYear()} - All right reserved by Chris Stallings</p><a href="https://www.linkedin.com/in/chriscms"><FaLinkedin /></a>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved by Chris Stallings</p>
+    <p><a href="https://www.linkedin.com/in/chriscms"><FaLinkedin /></a><a href=""></a><a href="https://github.com/chris-stallings"><FaGithub /></a></p>
   </aside>
 </footer>
   )
