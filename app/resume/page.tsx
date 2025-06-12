ff3aa12e480fb1c6d988e99b5907514cba7f4502
chris-stallings <164257968+chris-stallings@users.noreply.github.com>
2025-06-12 13:02:01 -0400

    fixed resume added download

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
+import { FaDownload } from 'react-icons/fa';
 
 export default function Resume() {
   return (
-    <div>
-      
-    </div>
+<div>
+  <div className="text-center">
+    Click the download button below to view my resume.
+  </div>
+  <div className="flex justify-center"><Link href="resume.pdf"><FaDownload size="50"/></Link></div>
+</div>
   );
 }
