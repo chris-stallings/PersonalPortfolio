1161a0b431ff58dc1a0340834b9218fc6be8b421
chris-stallings <164257968+chris-stallings@users.noreply.github.com>
2025-06-12 11:48:28 -0400

    fixed contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,10 +4,12 @@ import { FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 export default function Contact() {
   return (
-    <div>
+    <div className="text-center">
       <p>Want to get in touch? I am always open working on new projects or looking for new opportunities that advance my career.</p>
-      <p>Check out my <a href="https://www.linkedin.com/in/chriscms"><FaLinkedin /></a></p>
-      <p>Check out my <a href="https://github.com/chris-stallings"><FaGithub /></a></p>
+      <p>Check out my</p> 
+      <div className="flex justify-center"><a href="https://www.linkedin.com/in/chriscms"><FaLinkedin size="50"/></a></div>
+      <p>Check out my</p> 
+      <div className="flex justify-center"><a href="https://github.com/chris-stallings"><FaGithub size="50"/></a></div>
     </div>
   );
 }
